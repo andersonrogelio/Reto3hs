a21ce3a6e37c5f441844f17f068031b4567208cf
Anderson <andersonrogelio0@gmail.com>
2022-08-16 20:55:11 -0300

    Le cree los metodos que voy a implementar en mi servicio

--- src/main/java/com/example/reto/repository/IHouseRepository.java
+++ src/main/java/com/example/reto/repository/IHouseRepository.java
@@ -1,4 +1,14 @@
 package com.example.reto.repository;
 
+import com.example.reto.model.HouseModel;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface IHouseRepository {
+    Flux<HouseModel> addHouse(HouseModel house);
+    Flux<HouseModel> updateHouse(HouseModel house);
+    Flux<HouseModel> showAll();
+    Mono<HouseModel> showById();
+    Mono<Void> eliminar(Integer id);
+
 }
